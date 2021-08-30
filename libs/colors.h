5afdf52bc8dcc9c772e7e41cf515e76557c84c0b
Thiago Tenório <thiago.tenorio@rogalabs.com>
2021-08-30 14:39:15 -0300

    feat: define colors and vectors

---
+++ libs/colors.h
@@ -0,0 +1,13 @@
+#ifndef COLORS_FILE
+#define COLORS_FILE
+
+#define BLACK 0.0f, 0.0f, 0.0f
+#define RED 1.0f, 0.0f, 0.0f
+#define GREEN 0.0f, 1.0f, 0.0f
+#define BLUE 0.0f, 0.0f, 1.0f
+#define WHITE 1.0f, 1.0f, 1.0f
+#define YELLOW 1.0f, 1.0f, 0.0f
+#define PURPLE 1.0f, 0.0f, 1.0f
+#define CYAN 0.0f, 1.0f, 1.0f
+
+#endif
---
+++ libs/vectors.h
@@ -0,0 +1,12 @@
+#ifndef VECTORS_FILE
+#define VECTORS_FILE
+
+typedef struct Vec2D {
+	float x, y;
+} Vec2D;
+
+typedef struct Vec3D {
+	float x, y, z;
+} Vec3D;
+
+#endif
